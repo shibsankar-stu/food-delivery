2ad443284c3133617ba3aaa9b79b4d675d4c759f
shibsankar-stu <dasshibsankar94732@gmail.com>
2025-10-28 23:40:48 +0530

    add payment

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react'
-import { StoreContext } from '../../context/storeContext'
-import './cart.css'
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/storeContext";
+import "./cart.css";
+import { useNavigate } from "react-router-dom";
 const Cart = () => {
-
-  const {food_list,cartItems, removeFromCart, getTotalCartAmount } = useContext(StoreContext)
+  const { food_list, cartItems, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Item</p>
@@ -18,55 +20,57 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map((item, index) => {
           {
-            food_list.map((item, index) => {
-              {
-                if (cartItems[item._id] > 0) {
-                  return(
-                    <div className="cart-items-title cart-items-item">
-                    <img src={item.image} />
-                    <p>{item.name}</p>
-                    <p>₹{item.price}</p>
-                    <p>{cartItems[item._id]}</p>
-                    <p>₹{(item.price * cartItems[item._id])}</p>
-                    <button onClick={()=>removeFromCart(item._id)}>Remove</button>
-                    </div>
-                  )
-                }
-              }
-            })
+            if (cartItems[item._id] > 0) {
+              return (
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} />
+                  <p>{item.name}</p>
+                  <p>₹{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>₹{item.price * cartItems[item._id]}</p>
+                  <button onClick={() => removeFromCart(item._id)}>
+                    Remove
+                  </button>
+                </div>
+              );
+            }
           }
+        })}
       </div>
       <div className="cart-footer">
         <div className="cart-total">
           <h3> Cart Total </h3>
-        
-        <div className="cart-total-details">
-          <p> Subtotal </p>
-          <b> {getTotalCartAmount()} </b>
+
+          <div className="cart-total-details">
+            <p> Subtotal </p>
+            <b> {getTotalCartAmount()} </b>
+          </div>
+          <div className="cart-total-details">
+            <p>Delivery</p>
+            <b> {10} </b>
+          </div>
+          <div className="cart-total-details">
+            <p>Total</p>
+            <b> {getTotalCartAmount() + 10} </b>
+          </div>
+          <button onClick={() => navigate("/order")}>
+            Proceet to checkout
+          </button>
         </div>
-        <div className="cart-total-details">
-          <p>Delivery</p>
-          <b> {10} </b>
-        </div>
-        <div className="cart-total-details">
-          <p>Total</p>
-          <b> {getTotalCartAmount()+10} </b>
-        </div>
-        <button> Proceet to checkout </button>
-      </div>
-      <div className="cart-promocode">
-        <div>
-          <p>Ifyou have  a promo code, Enter it here</p>
-          <div className="cart-promocode-input">
-            <input type="text" placeholder='promo code' />
-            <button>Submit</button>
+        <div className="cart-promocode">
+          <div>
+            <p>Ifyou have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./placeorder.css";
+import { StoreContext } from "../../context/storeContext";
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
     <div>
-      <h1>Place Order</h1>
-    </div>
-  )
-}
+      <form>
+        <div className="place-order">
+          <div className="place-order-left">
+            <p>Delivery information </p>
+            <div className="multi-fields">
+              <input type="text" placeholder="First name" />
+              <input type="text" placeholder="last name" />
+            </div>
+            <div className="multi-fields">
+              <input type="email" placeholder="Email address" />
+              <input type="text" placeholder="Street" />
+            </div>
+            <div className="multi-fields">
+              <input type="text" placeholder="City" />
+              <input type="text" placeholder="State" />
+            </div>
+            <div className="multi-fields">
+              <input type="text" placeholder="Pin code" />
+              <input type="text" placeholder="Country" />
+            </div>
+            <input type="text" placeholder="Phone" />
+          </div>
+          <div className="place-order-left">
+            <div className="cart-total">
+              <h3> Cart Total </h3>
 
-export default PlaceOrder
+              <div className="cart-total-details">
+                <p> Subtotal </p>
+                <b> {getTotalCartAmount()} </b>
+              </div>
+              <div className="cart-total-details">
+                <p>Delivery</p>
+                <b> {10} </b>
+              </div>
+              <div className="cart-total-details">
+                <p>Total</p>
+                <b> {getTotalCartAmount() + 10} </b>
+              </div>
+              <button onClick={() => navigate("/order")}>
+                PROCEED TO PAYMENT
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PlaceOrder;
